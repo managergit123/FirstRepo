58d1da5585ef1db76c5f3fda16d332282136cac4
managergit123 <143540781+managergit123@users.noreply.github.com>
2025-04-14 08:26:52 -0700

    commit by Team member

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("First Git project");
 		System.out.println("Hello World");
 		System.out.println("Another statement");
+		System.out.println("stat added by team members");
 	}
 
 }
