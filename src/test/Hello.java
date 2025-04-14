e864e81634638ff8ae2ced1dce2500cbb1ef6b4f
managergit123 <143540781+managergit123@users.noreply.github.com>
2025-04-14 06:50:14 -0700

    second commit

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("First Git project");
 		System.out.println("Hello World");
+		System.out.println("Another statement");
 	}
 
 }
